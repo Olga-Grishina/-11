25fb01d43b1dadb4cd1eb954bc4e7f37fb9a42a1
Olga-Grishina <65589686+Olga-Grishina@users.noreply.github.com>
2020-05-20 13:13:15 +0500

    Add files via upload

---
+++ Zadacha11.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cstdlib> 
+using namespace std;// без этой строки не работает
+
+int main() {
+    long b, a;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите первое число  ";
+    cin >> a;
+    cout << "Введите второе число  ";
+    cin >> b;
+
+    while (b != a)
+    {
+        if (b > a)
+        {
+            b = b - a;
+        }
+        else
+        {
+            a = a - b;
+        }
+    }
+    cout << "Наибольший общий делитель =  ";
+    cout << b;
+}
